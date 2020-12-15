a249421e0f08b403bb710aff20f176616ebe736b
wojtekg4455 <wojtekg4455@gmail.com>
2020-12-15 16:13:35 +0100

    Komentarz
    Prościutki skrypt sterujący pompą oraz wiatrakami.

---
+++ pompa_fan.sh
@@ -0,0 +1,25 @@
+echo "Tryb pracy pompy wybierz : "
+echo "
+	Modes :
+		3 -Quiet
+		4 - Balanced
+		5 - Performancei
+podaj :"
+read;
+sudo OpenCorsairLink.elf --device 0 --pump mode=${REPLY}
+echo "Ustaw prędkość wiatraków :"
+echo "
+	Modes:
+		0 - Fixed PWM (requires to specify the PWM)
+		1 - Fixed RPM (requires to specify the RPM)
+		2 - Default
+		3 - Quiet
+		4 - Balanced
+		5 - Performance
+		6 - Custom Curve
+
+podaj :"
+read;
+sudo OpenCorsairLink.elf --device 0 --fan channel=0,mode=${REPLY}
+sudo OpenCorsairLink.elf --device 0 --fan channel=1,mode=${REPLY}
+sudo OpenCorsairLink.elf --device 0
